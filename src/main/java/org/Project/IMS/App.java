3c0611e76b5df51f2030e537a14c5b335a5d4feb
Admin <Admin@192.168.1.17>
2024-08-03 11:29:08 +0530

    first commit

---
+++ src/main/java/org/Project/IMS/App.java
@@ -0,0 +1,13 @@
+package org.Project.IMS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
